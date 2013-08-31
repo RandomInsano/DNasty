33190c53adeac5d51566a36b738a4ad011cb809d
Charlie Root <root@Testing.localdomain>
2013-08-31 16:53:40 -0500

    Fix build on OpenBSD with GCC

--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <netinet/in.h> // GCC stuffs.
+
 
 #include "utilities.h"
 #include "main.h"
--- resolver.c
+++ resolver.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
 
 #include "main.h"
 #include "resolver.h"
